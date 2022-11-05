72208a4da43d05917005261d901b3f2591da16c5
Cemal Salih Bulak <csbulak@gmail.com>
2022-11-05 12:42:23 +0300

    Filtering Process

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -3,7 +3,16 @@ import Form from "./Form/Form";
 import List from "./List/List";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "cemal",
+      phone_number: "987654321",
+    },
+    {
+      fullname: "sümeyye",
+      phone_number: "123456789",
+    },
+  ]);
 
   useEffect(() => {
     console.log(contacts);
--- src/components/Contacts/List/List.js
+++ src/components/Contacts/List/List.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
+
   return (
     <div>
+      <input
+        placeholder="filter contact"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
       <ul>
-        {contacts.map((item, i) => (
+        {filtered.map((item, i) => (
           <li key={i}>{item.fullname}</li>
         ))}
       </ul>
